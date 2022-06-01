bf697bd0f3e17e44ccc210549b0a04f586c6fe5c
TurquoiseTNT <69688534+TurquoiseTNT@users.noreply.github.com>
2022-06-01 15:20:42 +0100

    Adds in the Baloon

--- src/main/java/tk/turquoisetnt/tntextras/init/TntextrasModItems.java
+++ src/main/java/tk/turquoisetnt/tntextras/init/TntextrasModItems.java
@@ -5,6 +5,7 @@
 package tk.turquoisetnt.tntextras.init;
 
 import tk.turquoisetnt.tntextras.item.SilverCoinItem;
+import tk.turquoisetnt.tntextras.item.RedBallonItem;
 import tk.turquoisetnt.tntextras.item.GoldCoinItem;
 import tk.turquoisetnt.tntextras.item.EmptyColaCanItem;
 import tk.turquoisetnt.tntextras.item.ColaItem;
@@ -22,4 +23,5 @@ public class TntextrasModItems {
 	public static final RegistryObject<Item> SILVER_COIN = REGISTRY.register("silver_coin", () -> new SilverCoinItem());
 	public static final RegistryObject<Item> COLA = REGISTRY.register("cola", () -> new ColaItem());
 	public static final RegistryObject<Item> EMPTY_COLA_CAN = REGISTRY.register("empty_cola_can", () -> new EmptyColaCanItem());
+	public static final RegistryObject<Item> RED_BALLON = REGISTRY.register("red_ballon", () -> new RedBallonItem());
 }
---
+++ src/main/java/tk/turquoisetnt/tntextras/item/RedBallonItem.java
@@ -0,0 +1,35 @@
+
+package tk.turquoisetnt.tntextras.item;
+
+import tk.turquoisetnt.tntextras.procedures.RedBallonLevitationTrickProcedure;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.entity.Entity;
+
+public class RedBallonItem extends Item {
+	public RedBallonItem() {
+		super(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(64).rarity(Rarity.COMMON));
+	}
+
+	@Override
+	public UseAnim getUseAnimation(ItemStack itemstack) {
+		return UseAnim.EAT;
+	}
+
+	@Override
+	public int getUseDuration(ItemStack itemstack) {
+		return 0;
+	}
+
+	@Override
+	public void inventoryTick(ItemStack itemstack, Level world, Entity entity, int slot, boolean selected) {
+		super.inventoryTick(itemstack, world, entity, slot, selected);
+		if (selected)
+			RedBallonLevitationTrickProcedure.execute(world, entity.getX(), entity.getY(), entity.getZ());
+	}
+}
