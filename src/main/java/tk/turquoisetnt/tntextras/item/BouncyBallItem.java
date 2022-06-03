6358384334af9d636ccb78173a403d3a38598b54
TurquoiseTNT <sebas@SEB-DESKTOP-PC>
2022-06-03 09:14:50 +0100

    Made the BouncyBall Cool I will release the DEV KIT soon

--- src/main/java/tk/turquoisetnt/tntextras/item/BouncyBallItem.java
+++ src/main/java/tk/turquoisetnt/tntextras/item/BouncyBallItem.java
@@ -1,12 +1,15 @@
 
 package tk.turquoisetnt.tntextras.item;
 
+import tk.turquoisetnt.tntextras.procedures.BouncyBallItemInHandTickProcedure;
 import tk.turquoisetnt.tntextras.init.TntextrasModTabs;
 
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.Entity;
 
 public class BouncyBallItem extends Item {
 	public BouncyBallItem() {
@@ -21,5 +24,12 @@ public class BouncyBallItem extends Item {
 	@Override
 	public int getUseDuration(ItemStack itemstack) {
 		return 0;
+	}
+
+	@Override
+	public void inventoryTick(ItemStack itemstack, Level world, Entity entity, int slot, boolean selected) {
+		super.inventoryTick(itemstack, world, entity, slot, selected);
+		if (selected)
+			BouncyBallItemInHandTickProcedure.execute(world, entity.getX(), entity.getY(), entity.getZ());
 	}
 }
